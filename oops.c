add8d522653d0e3a4cb664b50bfcd4258bd61fe1
ssvikash_20 <arisivikash@gmail.com>
2025-05-10 19:40:29 +0530

    Follow the naming standards

--- oops.c
+++ oops.c
@@ -6,14 +6,14 @@ typedef struct Point
     int b;
 
     void (*hello)(struct Point*);
-} Point;
+} Point_t;
 
-void _hello(Point *p) {
+void _hello(Point_t *p) {
     printf("%d\n", p->a);
     printf("%d\n", p->b);
 }
 
-void _init_point(Point *p, int a, int b) {
+void _init_point(Point_t *p, int a, int b) {
     p->a = a;
     p->b = b;
     p->hello = _hello;
@@ -21,7 +21,7 @@ void _init_point(Point *p, int a, int b) {
 
 int main(int argc, char const *argv[])
 {
-    Point o;
+    Point_t o;
 
     _init_point(&o, 1, 2);
     o.hello(&o);
